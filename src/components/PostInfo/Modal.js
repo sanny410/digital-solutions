85ec75363717bf680d6f39cfd272f614b536f024
Evgeniya <74735099+sanny410@users.noreply.github.com>
2022-05-24 17:08:01 +0300

    refactor code

--- src/components/PostInfo/Modal.js
+++ src/components/PostInfo/Modal.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Modal = () => {
     return (
         <div className="modal__content load-success">
-            <h2 className="modal-title">Ваш комментарий отправлен</h2>
+            <h2 className="modal-title">Your comment has been sent</h2>
             <img id="icon-smile" src={require("../../assets/smile.png")} alt="" />
         </div>
     );
